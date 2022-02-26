b2b407c91e4795b0f80d2f704ea94a80150a471a
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2022-02-25 19:26:23 -0500

    Protección de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/']);
 
 const routes: Routes = [
   { path: '' ,loadChildren: () => import('./tienda/tienda.module').then( m => m.TiendaModule ) },
   { path: 'auth' ,loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ) },
-  { path: 'admin' ,loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule ) },
+  { 
+    path: 'admin' ,
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule ), 
+    canActivate: [ AngularFireAuthGuard ],
+    data: { authGuardPipe: redirectUnauthorizedToLogin } 
+  },
   { path: '**', redirectTo:''}
 ];
 
