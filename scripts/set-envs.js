e0d969d59e91ca51c6439cd0ca4981f0ffddb5f8
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-12-15 12:47:56 -0500

    Update env.template

--- scripts/set-envs.js
+++ scripts/set-envs.js
@@ -2,21 +2,21 @@ const { writeFileSync, mkdirSync } = require('fs');
 require('dotenv').config();
 
 const targetPath = './src/environments/environment.ts';
+const targetPathProd = './src/environments/environment.prod.ts';
 
-const envConfigFile = `
-export const environment = {
+const envConfigFile = `export const environment = {
   firebaseConfig: {
-    projectId: '${process.env.FIREBASE_PROJECT_ID}',
-    appId: '${process.env.FIREBASE_APP_ID}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-    locationId: '${process.env.FIREBASE_LOCATION_ID}',
     apiKey: '${process.env.FIREBASE_API_KEY}',
     authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
     databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
-  }
+    projectId: '${process.env.FIREBASE_PROJECT_ID}',
+    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
+    appId: '${process.env.FIREBASE_APP_ID}',
+  },
 };
 `;
 
 mkdirSync('./src/environments', { recursive: true });
 writeFileSync(targetPath, envConfigFile);
+writeFileSync(targetPathProd, envConfigFile);
