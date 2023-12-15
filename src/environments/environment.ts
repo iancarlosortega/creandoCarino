d73db8c354a1fc2a1f8f3acebc5a95b1e43608fd
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-12-15 12:27:57 -0500

    Fix firebaseConfig variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
-
 export const environment = {
-  firebaseConfig: {
-    projectId: 'creando-carino',
-    appId: '1:780338440755:web:874d6cbbb29cb4b2f99146',
-    storageBucket: 'creando-carino.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyADEYmQhpn8CTLtSJ61VgzQga4mRQBAfRQ',
-    authDomain: 'undefined',
-    messagingSenderId: '780338440755',
-  }
+	firebaseConfig: {
+		apiKey: 'AIzaSyADEYmQhpn8CTLtSJ61VgzQga4mRQBAfRQ',
+		authDomain: 'creando-carino.firebaseapp.com',
+		databaseURL: 'https://creando-carino-default-rtdb.firebaseio.com',
+		projectId: 'creando-carino',
+		storageBucket: 'creando-carino.appspot.com',
+		messagingSenderId: '780338440755',
+		appId: '1:780338440755:web:874d6cbbb29cb4b2f99146',
+	},
 };
