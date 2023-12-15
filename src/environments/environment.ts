652cab2f391d4a251dc12833032d0b1b92d15ae5
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-12-15 12:19:48 -0500

    Set firebaseConfig variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  firebaseConfig: {
+    projectId: 'creando-carino',
+    appId: '1:780338440755:web:874d6cbbb29cb4b2f99146',
+    storageBucket: 'creando-carino.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyADEYmQhpn8CTLtSJ61VgzQga4mRQBAfRQ',
+    authDomain: 'undefined',
+    messagingSenderId: '780338440755',
+  }
+};
