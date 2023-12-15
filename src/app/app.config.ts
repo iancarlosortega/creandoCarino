ffdfbe5b2c39fd0155be728bca8afc2644bbd75c
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-12-15 15:36:27 -0500

    Place defer blocks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -30,7 +30,17 @@ export const appConfig: ApplicationConfig = {
 		provideAnimations(),
 		provideToastr(),
 		importProvidersFrom([
-			provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+			provideFirebaseApp(() =>
+				initializeApp({
+					apiKey: 'AIzaSyADEYmQhpn8CTLtSJ61VgzQga4mRQBAfRQ',
+					authDomain: 'creando-carino.firebaseapp.com',
+					databaseURL: 'https://creando-carino-default-rtdb.firebaseio.com',
+					projectId: 'creando-carino',
+					storageBucket: 'creando-carino.appspot.com',
+					messagingSenderId: '780338440755',
+					appId: '1:780338440755:web:874d6cbbb29cb4b2f99146',
+				})
+			),
 			provideAuth(() => getAuth()),
 			provideFirestore(() => getFirestore()),
 			provideStorage(() => getStorage()),
